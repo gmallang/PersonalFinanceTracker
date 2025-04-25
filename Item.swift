9cb8eb06e407aa10b5b7ec40f0051490e343106e
Gnana Chand Mallangi <chintu13.mallangi@gmail.com>
2025-04-25 10:16:03 -0400

    Update and rename Item to Item.swift

--- Item.swift
+++ Item.swift
@@ -1,13 +1,12 @@
 import Foundation
-import SwiftData
 
-@Model
-class Item {
+struct Item: Identifiable, Codable, Hashable {
+    let id = UUID()
     var name: String
     var amount: Double
     var category: String
     var date: Date
-
+    
     init(name: String, amount: Double, category: String, date: Date = Date()) {
         self.name = name
         self.amount = amount
